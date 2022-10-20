2cfc5357ddb67bf0ef225b59430a7aedfba6b89f
njiti <kamaukan77@gmail.com>
2022-10-20 23:28:02 +0300

    added logging out feature

--- app/src/main/java/com/example/test_out/UserProfileActivity.java
+++ app/src/main/java/com/example/test_out/UserProfileActivity.java
@@ -7,6 +7,8 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -111,4 +113,51 @@ public class UserProfileActivity extends AppCompatActivity {
             }
         });
     }
+
+    //Creating ActionBar Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate menu items
+        getMenuInflater().inflate(R.menu.common_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //When any menu item is selected
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+         if(id == R.id.menu_refresh){
+             //Refresh Activity
+             startActivity(getIntent());
+             finish();
+             overridePendingTransition(0, 0);
+         } /*else if (id == R.id.menu_update_profile){
+             Intent intent = new Intent(UserProfileActivity.this, UpdateProfileActivity.class);
+             startActivity(intent);
+         } else if(id == R.id.menu_update_email) {
+             Intent intent = new Intent (UserProfileActivity.this, UpdateEmailActivity.class);
+             startActivity(intent);
+         }else if (id == R.id.menu_settings) {
+             Toast.makeText(UserProfileActivity.this, "menu_settings", Toast.LENGTH_SHORT).show();
+         } else if (id == R.id.menu_change_password){
+             Intent intent = new Intent(UserProfileActivity.this, ChangePasswordActivity.class);
+             startActivity(intent);
+         } else if (id == R.id.menu_delete_profile){
+             Intent intent = new Intent (UserProfileActivity.this, DeleteProfileActivity.class);
+             startActivity(intent);
+         } */else if (id == R.id.menu_logout){
+             authProfile.signOut();
+             Toast.makeText(UserProfileActivity.this, "Logged Out", Toast.LENGTH_SHORT).show();
+             Intent intent = new Intent(UserProfileActivity.this, MainActivity.class);
+
+             //Clear stack to prevent user coming back to UserProfileActivity on pressing back button after Logging out
+             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+             startActivity(intent);
+             finish(); //close UserProfileActivity
+         } else {
+             Toast.makeText(UserProfileActivity.this, "Something went wrong!", Toast.LENGTH_SHORT).show();
+         }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
